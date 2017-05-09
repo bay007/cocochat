b6541cbb305ee1bada85bb083225fc1e926e8bd1
egalicia <tnt.galicia@gmail.com>
2017-05-09 18:02:23 -0500

    fix: Eliminacion de archivos innecesarios

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 import ChatStore from './ChatStore';
 
 ReactDOM.render(
-  <App ChatStore={ChatStore}/>,
+  <App ChatStore={ChatStore} />,
   document.getElementById('coconnut-chat-widget')
 );
