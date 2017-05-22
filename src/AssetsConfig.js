4ce4299b5cbb16846602b12313255726c30a76c1
bay007 <tnt.galicia@gmail.com>
2017-05-22 11:02:27 -0500

    Add final buket S3

---
+++ src/AssetsConfig.js
@@ -0,0 +1,6 @@
+const AssetsConfig = {
+  baseAssetsURL: 'https://s3.amazonaws.com/aws-s3-us-east-1-boletia-assets-prod-01',
+  baseScriptsURL: 'https://s3.amazonaws.com/aws-s3-us-east-1-boletia-assets-prod-01/scripts'
+};
+
+export default AssetsConfig;
